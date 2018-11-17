93584861f3a816ae787fe91959f0c73b7f7cbefd
palindrom615 <palindrom615@hanmail.net>
2018-11-17 06:50:47 +0000

    Add go root into path

--- XDG_CONFIG_HOME/fish/conf.d/go.fish
+++ XDG_CONFIG_HOME/fish/conf.d/go.fish
@@ -1,9 +1,6 @@
-if type go > /dev/null 2>&1
-	# set the workspace path
-	set -x GOPATH $HOME/go
-	set -x GOROOT /usr/local/go
+# set the workspace path
+set -x GOPATH $HOME/go
+set -x GOROOT /usr/local/go
 
-	# add the go bin path to be able to execute our programs
-	set -x PATH $GOROOT/bin $GOPATH/bin $PATH
-end
-
+# add the go bin path to be able to execute our programs
+set -x PATH $GOROOT/bin $GOPATH/bin $PATH
