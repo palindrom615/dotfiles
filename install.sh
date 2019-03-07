aadfbfc85f8e981f6906a6dad5b8c91ad433dfa1
nick.615 <nick.615@nick615ui-MacBookPro.local>
2019-03-07 15:43:10 +0900

    Add work profiles

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 
+# macOS does not have coreutils out-of-box
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 REPODIR=$(realpath $(dirname "$0"))
 
 ln -s $REPODIR/XDG_CONFIG_HOME/* $HOME/.config
+
+#link whole files include name of which starts with dot
 ln -s $REPODIR/HOME/.[!.]* $HOME
 
