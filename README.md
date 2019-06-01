1eff028b5afcf9e678d7a809e846e675c12bd953
Jang Whemoon <palindrom615@gmail.com>
2019-06-01 16:14:16 +0900

    Add Windows support

---
+++ README.md
@@ -0,0 +1,5 @@
+# dotfiles of palindrom615(Jang Whemoon)
+
+* This repo is not for general use!
+* Install scripts guarantee idempotence.
+* The settings try to cover linux, macOS and Windows.
---
+++ install.ps1
@@ -0,0 +1,10 @@
+# For creating symbolic link with powershell, you should enable developer
+# features or execute this script with administrator previliege.
+
+Get-ChildItem $PSScriptRoot\XDG_CONFIG_HOME | ForEach-Object -Process {
+	New-Item -Path ~\.config -Name $_.Name -ItemType symboliclink -Value $_.FullName
+}
+
+Get-ChildItem $PSScriptRoot\HOME | ForEach-Object -Process {
+	New-Item -Path ~ -Name $_.Name -ItemType symboliclink -Value $_.FullName
+}
