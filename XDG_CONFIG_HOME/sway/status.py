3d7876c316a89abf9f7e97d63fd69564f9ea1caa
Jang Whemoon <palindrom615@gmail.com>
2019-12-29 18:54:52 +0900

    Fix status bar script

--- XDG_CONFIG_HOME/sway/status.py
+++ XDG_CONFIG_HOME/sway/status.py
@@ -12,98 +12,118 @@ except ImportError as e:
     print("'psutil' package not installed: do `pip install psutil`")
     sys.exit(1)
 
+"""
+see: `man swaybar-protocol`
+"""
+
 print('{"version": 1}')
 print('[')
 
 
-def charblock(chr, **kargs):
-    return {"full_text": chr, "separator": False, "separator_block_width": 0, **kargs}
+class Block():
+    def __init__(self, txt, **props):
+        self.full_text = txt
+        self.border = "#FFFFFF"
+        self.separator = False
+        self.separator_block_width = 0
+        for k in props:
+            setattr(self, k, props[k])
+
+    @staticmethod
+    def charblock(name, **kargs):
+        return Block(name, separator=False, **kargs)
+
+    @staticmethod
+    def numblock(num, **kargs):
+        return Block(num, border_left=0, border_right=0, **kargs)
 
 
-def numblock(num, rjust=0, **kargs):
-    return {"full_text": num.rjust(rjust), "align": "right", **kargs}
+def blocks(name, *contents, props={}):
+    name = Block.charblock(name, **props)
+    content_blocks = [content if isinstance(
+        content, Block) else Block.numblock(content, **props) for content in contents]
+    content_blocks[-1].border_right = 1
+    content_blocks[-1].separator_block_width = 4
+    return [name, *content_blocks]
 
 
 def brightness():
     process = subprocess.Popen(["brightnessctl", "-m"], stdout=subprocess.PIPE)
     _, _, current_brightness, current_percentage, max_brightness = str(
         process.communicate()[0])[0:-3].split(',')
-    return [charblock("明"), numblock(f"{current_percentage}", rjust=4)]
+    return blocks("明", f"{current_percentage:>4}")
 
 
 def cpu():
-    cpu_percent = psutil.cpu_percent()
-    return [charblock("核"), numblock(f"{cpu_percent}%", rjust=6)]
+    cpu_percent = psutil.cpu_percent(interval=1)
+    return blocks("核", f"{cpu_percent:5}%")
 
 
 def mem():
     vmem = psutil.virtual_memory()
     mem_percent = vmem.available / vmem.total
-    return [charblock("記"), numblock(f"{mem_percent:.1%}", rjust=6)]
+    return blocks("記", f"{mem_percent:6.1%}")
 
 
 def today():
     w = "月火水木金土日"[time.localtime().tm_wday]
-    return [charblock(w), numblock(time.strftime(f"%m.%d"), rjust=6)]
+    return blocks(w, time.strftime("%m.%d").rjust(6))
 
 
 def now():
-    return [charblock("時"), numblock(time.strftime("%H%M"), rjust=5)]
+    return blocks("時", time.strftime("%H%M").rjust(5))
 
 
 def battery():
-    battery_percent = int(psutil.sensors_battery().percent)
-    return [charblock('電'), numblock(f"{battery_percent}%", rjust=5)]
+    battery_stats = psutil.sensors_battery()
+    percentage = battery_stats.percent
+    power_plugged = battery_stats.power_plugged
+    background = "#BE132D" if power_plugged else None
+    return blocks('電', f"{percentage:3.0f}%", props={"background": background})
 
 
-def net():
-
-    def check_interface(interface):
-        interface_addrs = psutil.net_if_addrs().get(interface) or []
-        return socket.AF_INET in [snicaddr.family for snicaddr in interface_addrs]
+def net_block(name, intfc):
+    interface_addrs = psutil.net_if_addrs().get(intfc) or []
+    is_working = socket.AF_INET in [
+        snicaddr.family for snicaddr in interface_addrs]
+    not_working = Block.numblock("不通", color="#ffffff55")
 
     def human_readable_size(size, decimal_places=0):
         for unit in ['B', 'K', 'M', 'G', 'T']:
             if size < 1024.0:
                 break
             size /= 1024.0
-        return f"{size:.{decimal_places}f}{unit}".rjust(5)
+        return f"{size:4.{decimal_places}f}{unit}"
 
-    def working(io):
-        up = charblock(
-            f"上{human_readable_size(io[0])}", color="#ffffff55" if io[0] == 0 else None)
-        down = numblock(
-            f"下{human_readable_size(io[1])}", color="#ffffff55" if io[1] == 0 else None)
-        return [up, down]
+    def color(x): return "ffffff55" if x == 0 else None
 
     def acc_sub(old, new):
         return ((new.bytes_sent-old[0], new.bytes_recv - old[1]), (new.bytes_sent, new.bytes_recv))
-
-    wired = "enp0s31f6"
-    wireless = "wlp2s0"
-    not_working = numblock("不通", rjust=3, color="#ffffff55")
-    wired_acc = (0, 0)
-    wireless_acc = (0, 0)
+    acc = (0, 0)
     while True:
-        wired_bytes, wired_acc = acc_sub(
-            wired_acc, psutil.net_io_counters(pernic=True)[wired])
-        wireless_bytes, wireless_acc = acc_sub(
-            wireless_acc, psutil.net_io_counters(pernic=True)[wireless])
-        wired_status = working(wired_bytes) if check_interface(
-            wired) else [not_working]
-        wireless_status = working(wireless_bytes) if check_interface(
-            wireless) else [not_working]
-        yield [charblock("波 "), *wireless_status, charblock("線 "), *wired_status]
+        new = psutil.net_io_counters(pernic=True)[intfc]
+        up = new.bytes_sent - acc[0]
+        down = new.bytes_recv - acc[1]
+
+        up = Block.numblock(
+            f"上{human_readable_size(up)}", color=color(up))
+        down = Block.numblock(
+            f"下{human_readable_size(down)}", color=color(down))
+        acc = (new.bytes_sent, new.bytes_recv)
+        yield blocks(name, up, down if is_working else not_working)
 
 
-net_gen = net()
+net_wired_gen = net_block("線", "enp12s0u1")
+net_wireless_gen = net_block("波", "wlp2s0")
 
 
 def sys_info():
-    return json.dumps([*brightness(), *next(net_gen), *cpu(), *mem(), *battery(), *today(), *now()])
+    blocks = [*brightness(), *next(net_wired_gen), *next(net_wireless_gen),
+              *cpu(), *mem(), *battery(), *today(), *now()]
+    return [block.__dict__ for block in blocks]
 
 
 starttime = time.time()
 while True:
-    print(sys_info(), end=",", flush=True)
+    print(json.dumps(sys_info()), end=",", flush=True)
     time.sleep(1. - ((time.time() - starttime) % 1.))
