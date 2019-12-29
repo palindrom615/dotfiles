bd2d574f957ab6213135201bfbb4e849c8e1f37e
Jang Whemoon <palindrom615@gmail.com>
2019-12-29 20:37:13 +0900

    Refactor status.py

--- XDG_CONFIG_HOME/sway/status.py
+++ XDG_CONFIG_HOME/sway/status.py
@@ -12,15 +12,40 @@ except ImportError as e:
     print("'psutil' package not installed: do `pip install psutil`")
     sys.exit(1)
 
-"""
-see: `man swaybar-protocol`
-"""
-
 print('{"version": 1}')
 print('[')
 
 
+class Interval():
+    """
+    This decorator class enables renewal interval configurable per block.
+    The class memoizes one latest value of function until time-to-live runs out.
+    """
+
+    def __init__(self, ttl=1):
+        self.ttl = ttl
+        self.value = None
+        self.key = None
+        self.expires = 0
+
+    def __call__(self, fn, *args, **kwargs):
+        def wrapped_func(*args, **kwargs):
+            key = tuple(args) + tuple(sorted(kwargs.items()))
+            if key != self.key or time.time() > self.expires:
+                self.key = key
+                self.value = fn(*args, **kwargs)
+                self.expires = time.time() + self.ttl
+            return self.value
+        return wrapped_func
+
+
 class Block():
+    """
+    This class represents one body block element of swaybar-protocol.
+    see: `man swaybar-protocol`
+    or https://github.com/swaywm/sway/blob/master/swaybar/swaybar-protocol.7.scd
+    """
+
     def __init__(self, txt, **props):
         self.full_text = txt
         self.border = "#FFFFFF"
@@ -30,63 +55,68 @@ class Block():
             setattr(self, k, props[k])
 
     @staticmethod
-    def charblock(name, **kargs):
+    def head_block(name, **kargs):
         return Block(name, separator=False, **kargs)
 
     @staticmethod
-    def numblock(num, **kargs):
+    def body_block(num, **kargs):
         return Block(num, border_left=0, border_right=0, **kargs)
 
-
-def blocks(name, *contents, props={}):
-    name = Block.charblock(name, **props)
-    content_blocks = [content if isinstance(
-        content, Block) else Block.numblock(content, **props) for content in contents]
-    content_blocks[-1].border_right = 1
-    content_blocks[-1].separator_block_width = 4
-    return [name, *content_blocks]
+    @staticmethod
+    def blocks(name, *bodies, props={}):
+        head = Block.head_block(name, **props)
+        body_blocks = [body if isinstance(body, Block) else
+                       Block.body_block(body, **props) for body in bodies]
+        body_blocks[-1].border_right = 1
+        body_blocks[-1].separator_block_width = 4
+        return [head, *body_blocks]
 
 
 def brightness():
     process = subprocess.Popen(["brightnessctl", "-m"], stdout=subprocess.PIPE)
     _, _, current_brightness, current_percentage, max_brightness = str(
         process.communicate()[0])[0:-3].split(',')
-    return blocks("明", f"{current_percentage:>4}")
+    return Block.blocks("明", f"{current_percentage:>4}")
 
 
+@Interval(4)
 def cpu():
     cpu_percent = psutil.cpu_percent(interval=1)
-    return blocks("核", f"{cpu_percent:5}%")
+    return Block.blocks("核", f"{cpu_percent:5}%")
 
 
+@Interval(4)
 def mem():
     vmem = psutil.virtual_memory()
     mem_percent = vmem.available / vmem.total
-    return blocks("記", f"{mem_percent:6.1%}")
+    return Block.blocks("記", f"{mem_percent:6.1%}")
 
 
+@Interval(60)
 def today():
     w = "月火水木金土日"[time.localtime().tm_wday]
-    return blocks(w, time.strftime("%m.%d").rjust(6))
+    return Block.blocks(w, time.strftime("%m.%d").rjust(5))
 
 
+@Interval(4)
 def now():
-    return blocks("時", time.strftime("%H%M").rjust(5))
+    return Block.blocks("時", time.strftime("%H:%M").rjust(5))
 
 
+@Interval(60)
 def battery():
     battery_stats = psutil.sensors_battery()
     percentage = battery_stats.percent
     power_plugged = battery_stats.power_plugged
     background = "#BE132D" if power_plugged else None
-    return blocks('電', f"{percentage:3.0f}%", props={"background": background})
+    return Block.blocks('電', f"{percentage:3.0f}%", props={"background": background})
 
 
 def net_block(name, intfc):
     interface_addrs = psutil.net_if_addrs().get(intfc) or []
     is_working = socket.AF_INET in [
         snicaddr.family for snicaddr in interface_addrs]
-    not_working = Block.numblock("不通", color="#ffffff55")
+    not_working = Block.body_block("不通", color="#ffffff55")
 
     def human_readable_size(size, decimal_places=0):
         for unit in ['B', 'K', 'M', 'G', 'T']:
@@ -97,20 +127,18 @@ def net_block(name, intfc):
 
     def color(x): return "ffffff55" if x == 0 else None
 
-    def acc_sub(old, new):
-        return ((new.bytes_sent-old[0], new.bytes_recv - old[1]), (new.bytes_sent, new.bytes_recv))
     acc = (0, 0)
     while True:
         new = psutil.net_io_counters(pernic=True)[intfc]
         up = new.bytes_sent - acc[0]
         down = new.bytes_recv - acc[1]
 
-        up = Block.numblock(
+        up = Block.body_block(
             f"上{human_readable_size(up)}", color=color(up))
-        down = Block.numblock(
+        down = Block.body_block(
             f"下{human_readable_size(down)}", color=color(down))
         acc = (new.bytes_sent, new.bytes_recv)
-        yield blocks(name, up, down if is_working else not_working)
+        yield Block.blocks(name, up, down if is_working else not_working)
 
 
 net_wired_gen = net_block("線", "enp12s0u1")
