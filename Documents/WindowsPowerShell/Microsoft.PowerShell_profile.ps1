60210f8e7252636d87c3bc66e7ea5345acd01c56
Jang Whemoon <palindrom615@gmail.com>
2023-08-13 14:10:27 +0900

    Update powershell kubeconfig files

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,9 @@
+$kubeConfDirectory = "$env:USERPROFILE\.kube\conf.d"
+$kubeConfigFiles = Get-ChildItem -Path $kubeConfDirectory
+$kubeConfigPaths = $kubeConfigFiles.FullName -join ";"
+
+$Env:KUBECONFIG="$kubeConfigPaths;$HOME\.kube\config"
+
 if(Get-Command 'starship' -errorAction SilentlyContinue) {
     Invoke-Expression (&starship init powershell)
 }
